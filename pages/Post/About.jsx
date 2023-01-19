a9d041eafce78a5b9e17776a9d24ccf88c2f382d
lady vera <ifebuchejulia@gmail.com>
2023-01-18 21:58:37 -0600

    19/1/23

--- pages/Post/About.jsx
+++ pages/Post/About.jsx
@@ -14,18 +14,21 @@ export default function About() {
       </Head>
       <div className={`min-w-screen ${styles.Header}`}>
         <p className=" flex-1 ">Nurika</p>
-        <ol className="list-none sm:flex hidden justify-end items-center flex-1 ">
+        {/* <ol className="list-none sm:flex hidden justify-end items-center flex-1 ">
           <li className='mr-10' ><Link href="/">home</Link></li>
           <li className='mr-10' ><Link href="/Post/Whitepaper">Whitepaper</Link></li>
           <li className='mr-10'><Link href="/Post/Community">Community</Link></li>
           <li className='mr-10'><Link href="/Post/
           Team">Team</Link></li>
-        </ol>
-        <button className='text-pink-800' >Join the waitlist</button>
-      </div>
+        </ol> */}
+        {/*  className=' sm:flex start justify-end items-center w-[25px] flex-1 ' */}
+        <button  >Join the waitlist</button>
+      </div>    
       <div className={styles.container_fluid}>
+      <div className ="text-white text-center text-[150px] font-normal font-lato"> It Pays To 
+         Walk</div>
         <div className={`text-black-900 text-[50px] min-w-screen min-h-screen mx-auto flex items-center justify-center ${styles.AppGlass1}`}>
-        <div className={styles.textAbout}> It Pays To Walk </div>
+          
         <Image
               src={runningLady}
               alt="runningLady"
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css';
+import Head from 'next/head'
 
-export default function App({Component, pageProps}){
-    return (
-        <Component{...pageProps}/>
-    )
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
