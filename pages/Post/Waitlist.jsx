7d1f84df2097e8824dcd749d5f55345879c4241e
lady vera <ifebuchejulia@gmail.com>
2023-01-20 03:52:21 -0600

    After this I'm starting responsiveness

--- pages/Post/Waitlist.jsx
+++ pages/Post/Waitlist.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import { socialMedia,footerLinks } from "../constants";
+import { socialMedia, footerLinks } from '../Post/constants'
 // import styles from './styles/Home.module.css';
 // import styles from './styles/Home.module.css';
 
@@ -14,28 +14,28 @@ export default function FirstPost() {
         <Link href="/">← Back to home</Link>
       </h2>
       <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
-                    {footerLinks.map((footerLink) => (
-                        <div
-                            key={footerLink.key}
-                            className='flex flex-col ss:my-10 my-4 min-w-[150px]'>
-                            <h4 className='font-poppins font-semibold text-[18x] leading-[27px] text-black' >
-                                {footerLink.title}
-                            </h4>
-                            <ul>
-                                {footerLink.links.map((link, index) => (
-                                    <li
-                                        key={link.name}
-                                        className={`font-poppins font-normal text-[16px] leading-[24px] text-black hover:text-secondary cursor-pointer ${index !== footerLink.length - 1 ? 'mb-4' : 'mb-0'} `}
-                                    >
-                                        {link.name}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
-                </div>
-            
-            
-              </section >
+        {footerLinks.map((footerLink) => (
+          <div
+            key={footerLink.key}
+            className='flex flex-col ss:my-10 my-4 min-w-[150px]'>
+            <h4 className='font-poppins font-semibold text-[18x] leading-[27px] text-black' >
+              {footerLink.title}
+            </h4>
+            <ul>
+              {footerLink.links.map((link, index) => (
+                <li
+                  key={link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-black hover:text-secondary cursor-pointer ${index !== footerLink.length - 1 ? 'mb-4' : 'mb-0'} `}
+                >
+                  {link.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+
+
+    </section >
   );
 }
